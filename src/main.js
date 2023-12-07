a1234d38329cb65d553e04112b62c899354a07b6
Mark Ghian Francis <zetsuya6167@gmail.com>
2023-12-07 23:25:33 +0800

    Finished header responsiveness and finish styling it

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import '../styles/style.css'
-import '../styles/modernize.css'
-import '../styles/utility.css'
-import '../styles/components/header.css'
+import '../styles/modernize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/utility.css';
