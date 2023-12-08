163a1b789741407016070d5328b75bb14e4742cc
Mark Ghian Francis <zetsuya6167@gmail.com>
2023-12-08 16:28:08 +0800

    Finished about section ( Style / Responsiveness )

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import '../styles/modernize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/about.css';
 import '../styles/utility.css';
