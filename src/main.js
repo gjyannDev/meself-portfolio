a06bc5a21b17753753e5b2abe87efa69df05a356
Mark Ghian Francis <zetsuya6167@gmail.com>
2023-12-08 23:38:38 +0800

    added form in contact section ( Finish it tommorow )

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/services.css';
 import '../styles/components/project.css';
+import '../styles/components/contact.css';
 import '../styles/utility.css';
