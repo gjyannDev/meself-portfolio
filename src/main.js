1b17d0e9cb981e13199b37a10cb254340208ed2b
Mark Ghian Francis <zetsuya6167@gmail.com>
2023-12-12 08:23:36 +0800

    Added utils folder in src and put all the utils js on to it

--- src/main.js
+++ src/main.js
@@ -9,13 +9,13 @@ import '../styles/components/contact.css';
 import '../styles/components/footer.css';
 import '../styles/animations.css';
 import '../styles/utility.css';
-import '../src/attractHover.js';
-import '../src/scrollAnimation.js';
-import '../src/imageHover.js';
-import '../src/typeWrite.js';
-import '../src/backToTop.js';
-import '../src/changeBackground.js';
-import lazyLoading from '/src/lazyLoading';
+import '../src/utils/attractHover.js';
+import '../src/utils/scrollAnimation.js';
+import '../src/utils/imageHover.js';
+import '../src/utils/typeWrite.js';
+import '../src/utils/backToTop.js';
+import '../src/utils/changeBackground.js';
+import lazyLoading from '/src/utils/lazyLoading';
 
 lazyLoading();
 
